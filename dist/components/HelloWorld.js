030e88386dc47fe5dec2c0c7141c888c9ec43042
Ildar Meyker <ildar.meyker@gmail.com>
2024-07-22 21:15:55 +0300

    Update.

--- dist/components/HelloWorld.js
+++ dist/components/HelloWorld.js
@@ -1,5 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import React from "react";
 var HelloWorld = function () {
-    return (React.createElement("div", null, "Hello, world. I am component from react-component-as-package."));
+    return (_jsx("div", { children: "Hello, world. I am component from react-component-as-package." }));
 };
 export default HelloWorld;
