e78422cc3647204d9a8b396828a6d2f4a4f13c08
Ildar Meyker <ildar.meyker@gmail.com>
2024-07-22 20:02:41 +0300

    Update.

---
+++ dist/components/HelloWorld.js
@@ -0,0 +1,5 @@
+import React from "react";
+var HelloWorld = function () {
+    return (React.createElement("div", null, "Hello, world. I am component from react-component-as-package."));
+};
+export default HelloWorld;
